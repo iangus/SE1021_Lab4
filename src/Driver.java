c6edd6fbcd2d89e357c3f635e9539e446ed4e22b
Ian Guswiler <iangus97@gmail.com>
2016-01-11 19:19:08 -0600

    Horse and chicken completed. Lots of the same code; Animal abstract class may be added. Gold and Silver completed. Also lots of the same code definitely needs abstract classes. Portfolio completed. TODO: Update Driver with user input (Possibly gui). Done with console input Implement abstract Animal and Metal classes. Clean up formatting and repeated code. Add javaDoc comments.

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,7 @@
 import edu.msoe.se1021.lab4.WavPlayer;
 
+import java.util.Scanner;
+
 /**
  * SE1021 - 032
  * Winter 2016
@@ -8,13 +10,45 @@ import edu.msoe.se1021.lab4.WavPlayer;
  * Created: 1/9/2016
  */
 public class Driver {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void horseCheck(Portfolio items){
+        System.out.println("How many horses?");
+        int numberOfHorses = input.nextInt();
+        items.addHorses(numberOfHorses);
+    }
+
+    public static void layerCheck(Portfolio items){
+        System.out.println("How many egg laying chickens?");
+        int numberOfLayers = input.nextInt();
+        items.addChickens(numberOfLayers, "egg layer");
+    }
+
+    public static void broilerCheck(Portfolio items){
+        System.out.println("How many broiler chickens?");
+        int numberOfBroilers = input.nextInt();
+        items.addChickens(numberOfBroilers, "broiler");
+    }
+
+    public static void goldCheck(Portfolio items){
+        System.out.println("How many gold bars?");
+        int numberOfGold = input.nextInt();
+        items.addGold(numberOfGold, 0.5);
+    }
+
+    public static void silverCheck(Portfolio items){
+        System.out.println("How many silver bars?");
+        int numberOfSilver = input.nextInt();
+        items.addSilver(numberOfSilver, 1.0);
+    }
+
     public static void main(String[] args) {
         Portfolio items = new Portfolio();
-        items.addHorses(4);
-        items.addChickens(3, "broiler");
-        items.addChickens(2, "egg layer");
-        items.addGold(4, 0.5);
-        items.addSilver(6, 1.0);
+        horseCheck(items);
+        layerCheck(items);
+        broilerCheck(items);
+        goldCheck(items);
+        silverCheck(items);
         items.printItems();
     }
 }
